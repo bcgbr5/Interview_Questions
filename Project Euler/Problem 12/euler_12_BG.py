5077601b63d87e2513491caba45de939c2bb1f23
Brandon Greer <brandonchrisgreer@gmail.com>
2019-11-23 16:11:01 -0600

    division is expensive

---
+++ Project Euler/Problem 12/euler_12_BG.py
@@ -0,0 +1,22 @@
+#Project Euler Problem 12
+#Solution : Brandon Greer
+import math
+def get_number_of_factors(number):
+    factors = 0
+    for i in range(1, int(math.sqrt(number))):
+        if number % i == 0:
+            factors += 2
+    print(factors)
+    return factors
+
+def get_triangular_number_with_n_factors(n):
+    triangular_num = 1
+    i = 2
+    while get_number_of_factors(triangular_num) < n:
+        triangular_num += i
+        i+=1
+        print(triangular_num)
+    return triangular_num
+
+print(get_triangular_number_with_n_factors(500))
+
